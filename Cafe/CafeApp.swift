00aea26025efc0c21e1bfd59ed63d1e60bca44d6
rlrlawodud97 <rlrlaowdud97@naver.com>
2022-11-25 18:23:33 +0900

    Initial Commit

---
+++ Cafe/CafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeApp.swift
+//  Cafe
+//
+//  Created by 김재영 on 2022/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
